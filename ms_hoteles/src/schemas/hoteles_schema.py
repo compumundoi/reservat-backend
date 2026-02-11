416cd6183df4ad4cd33fd2690e593afaaa9fd647
Deiver Julio <deiver-jc@live.com>
2026-02-10 21:46:56 -0500

    refactor: update hotel and provider schemas to include optional fields and default values.

--- ms_hoteles/src/schemas/hoteles_schema.py
+++ ms_hoteles/src/schemas/hoteles_schema.py
@@ -4,47 +4,47 @@ from datetime import datetime, time
 from uuid import UUID
 
 class DatosProveedor(BaseModel):
-    tipo: str
-    nombre: str
-    descripcion: str
-    email: str
-    telefono: str
-    direccion: str
-    ciudad: str
-    pais: str
-    sitio_web: str
-    rating_promedio: int 
-    verificado: bool
-    fecha_registro: datetime
-    ubicacion: str
-    redes_sociales: str
-    relevancia: str
-    usuario_creador: str
-    tipo_documento: str
-    numero_documento: str         
-    activo: bool  
+    tipo: Optional[str] = None
+    nombre: Optional[str] = None
+    descripcion: Optional[str] = None
+    email: Optional[str] = None
+    telefono: Optional[str] = None
+    direccion: Optional[str] = None
+    ciudad: Optional[str] = None
+    pais: Optional[str] = None
+    sitio_web: Optional[str] = None
+    rating_promedio: Optional[float] = 0 
+    verificado: bool = True
+    fecha_registro: Optional[datetime] = None
+    ubicacion: Optional[str] = None
+    redes_sociales: Optional[str] = None
+    relevancia: Optional[str] = None
+    usuario_creador: Optional[str] = None
+    tipo_documento: Optional[str] = None
+    numero_documento: Optional[str] = None         
+    activo: bool = True
 
 class DatosHotel(BaseModel):
-    estrellas: int
-    numero_habitaciones: int
-    servicios_incluidos: str
-    check_in: time
-    check_out: time
-    admite_mascotas: bool
-    tiene_estacionamiento: bool
-    tipo_habitacion: str
-    precio_ascendente:  float  
-    servicio_restaurante: bool
-    recepcion_24_horas: bool
-    bar: bool
-    room_service: bool
-    asensor: bool
-    rampa_discapacitado: bool
-    pet_friendly: bool
-    auditorio: bool
-    parqueadero: bool
-    piscina: bool
-    planta_energia: bool
+    estrellas: Optional[int] = 0
+    numero_habitaciones: Optional[int] = 0
+    servicios_incluidos: Optional[str] = None
+    check_in: Optional[time] = None
+    check_out: Optional[time] = None
+    admite_mascotas: bool = False
+    tiene_estacionamiento: bool = False
+    tipo_habitacion: Optional[str] = None
+    precio_ascendente: Optional[float] = 0.0  
+    servicio_restaurante: bool = False
+    recepcion_24_horas: bool = False
+    bar: bool = False
+    room_service: bool = False
+    asensor: bool = False
+    rampa_discapacitado: bool = False
+    pet_friendly: bool = False
+    auditorio: bool = False
+    parqueadero: bool = False
+    piscina: bool = False
+    planta_energia: bool = False
 
 class CrearHotelRequest(BaseModel):
     proveedor: DatosProveedor
@@ -54,48 +54,48 @@ class CrearHotelRequest(BaseModel):
 
 class ListarDatosProveedor(BaseModel):
     id_proveedor: UUID
-    tipo: str
-    nombre: str
-    descripcion: str
-    email: str
-    telefono: str
-    direccion: str
-    ciudad: str
-    pais: str
-    sitio_web: str
-    rating_promedio: int 
-    verificado: bool
-    fecha_registro: datetime
-    ubicacion: str
-    redes_sociales: str
-    relevancia: str
-    usuario_creador: str
-    tipo_documento: str
-    numero_documento: str         
-    activo: bool  
+    tipo: Optional[str] = None
+    nombre: Optional[str] = None
+    descripcion: Optional[str] = None
+    email: Optional[str] = None
+    telefono: Optional[str] = None
+    direccion: Optional[str] = None
+    ciudad: Optional[str] = None
+    pais: Optional[str] = None
+    sitio_web: Optional[str] = None
+    rating_promedio: Optional[float] = 0 
+    verificado: bool = True
+    fecha_registro: Optional[datetime] = None
+    ubicacion: Optional[str] = None
+    redes_sociales: Optional[str] = None
+    relevancia: Optional[str] = None
+    usuario_creador: Optional[str] = None
+    tipo_documento: Optional[str] = None
+    numero_documento: Optional[str] = None         
+    activo: bool = True
 
 class ListarDatosHotel(BaseModel):
     id_hotel: UUID
-    estrellas: int
-    numero_habitaciones: int
-    servicios_incluidos: str
-    check_in: time
-    check_out: time
-    admite_mascotas: bool
-    tiene_estacionamiento: bool
-    tipo_habitacion: str
-    precio_ascendente:  float  
-    servicio_restaurante: bool
-    recepcion_24_horas: bool
-    bar: bool
-    room_service: bool
-    asensor: bool
-    rampa_discapacitado: bool
-    pet_friendly: bool
-    auditorio: bool
-    parqueadero: bool
-    piscina: bool
-    planta_energia: bool
+    estrellas: Optional[int] = 0
+    numero_habitaciones: Optional[int] = 0
+    servicios_incluidos: Optional[str] = None
+    check_in: Optional[time] = None
+    check_out: Optional[time] = None
+    admite_mascotas: bool = False
+    tiene_estacionamiento: bool = False
+    tipo_habitacion: Optional[str] = None
+    precio_ascendente: Optional[float] = 0.0  
+    servicio_restaurante: bool = False
+    recepcion_24_horas: bool = False
+    bar: bool = False
+    room_service: bool = False
+    asensor: bool = False
+    rampa_discapacitado: bool = False
+    pet_friendly: bool = False
+    auditorio: bool = False
+    parqueadero: bool = False
+    piscina: bool = False
+    planta_energia: bool = False
 
 class ListarHotelResponse(BaseModel):
     proveedor: ListarDatosProveedor
